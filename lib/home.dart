d2cb60c34563b35de31e7a41b5836d467d9eab8e
BipinL <104549520+BipinL@users.noreply.github.com>
2022-06-16 20:20:27 +0545

    pages added

---
+++ lib/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Homeview extends StatelessWidget {
+  const Homeview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(),
+      body: const Text('hello world'),
+    ));
+  }
+}
